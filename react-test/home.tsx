4cc68f591fd48f0e5210db373ef861d8115766d9
Player1041 <redacteddoesit@gmail.com>
2024-11-22 23:32:19 +0000

    push new files

--- react-test/home.tsx
+++ react-test/home.tsx
@@ -1,6 +1,5 @@
-/** @jsxImportSource https://esm.sh/react@18.3.1 */
-import { createRoot } from "https://esm.sh/react-dom@18.3.1/client";
-
+import { React } from 'react';
+import { createRoot } from 'react-dom';
 
 function MyButton() {
   return (
@@ -13,10 +12,10 @@ function MyButton() {
 const MyApp = () => {
   return (
     <div>
-      <h1>Welcome to my app</h1>
+      <h1>Welcome to my</h1>
       <MyButton />
     </div>
   );
 }
 
-createRoot(document.getElementById("root")!).render(<MyApp />);
+export default MyApp
