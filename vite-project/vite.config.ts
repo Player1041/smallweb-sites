75e917bc7facb3af1261de7dd558758316e17a69
Player1041 <redacteddoesit@gmail.com>
2024-11-22 23:55:58 +0000

    updates

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import deno from '@deno/vite-plugin'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
